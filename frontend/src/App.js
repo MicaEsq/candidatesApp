b27ed173fae69d1117389e7cc391a8c9a45b3632
MicaEsq <micaesquerdo1@gmail.com>
2023-07-14 17:14:07 -0300

    Navbar and color changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
 import * as React from "react";
 import { Routes, Route, Outlet, Link } from "react-router-dom";
 import TableCandidates from './components/tableCandidates';
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <div>
       <Routes>
-        <Route path="/">
-          <Route path="/candidates" element={
+        <Route path="/" element={
               <>
-                <div><nav><ul><li><Link to="/">Home</Link></li><li><Link to="/candidates">Candidates</Link></li></ul></nav><hr /><Outlet /></div>
+                <Navbar></Navbar>
                 <TableCandidates></TableCandidates>
               </>
           } />
-        </Route>
+        <Route path="/newCandidate" element={
+            <>
+              <Navbar></Navbar>
+            </>
+        } />
       </Routes>
     </div>
   );
